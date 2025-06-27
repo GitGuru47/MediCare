e5e195c449fc989be895c2f67a3bfced4ee6db79
Kritika pradhan <108870896+GitGuru47@users.noreply.github.com>
2025-06-28 01:52:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+/* FILE: script.js */
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("contact-form");
+  const msg = document.getElementById("form-message");
+
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
+      msg.textContent = "Thank you! We'll get back to you shortly.";
+      form.reset();
+    });
+  }
+});
